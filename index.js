0eb566eec11bde11cf76d939666029df030cadf9
Stephan Seidt <evilhackerdude@gmail.com>
2014-06-06 16:00:18 +0200

    Moved from contentful.js

---
+++ index.js
@@ -0,0 +1,40 @@
+'use strict';
+
+var _ = require('lodash-contrib');
+
+module.exports = resolveResponse;
+
+function resolveResponse(response) {
+  walkMutate(response, isLink, function(link) {
+    return getLink(response, link) || link;
+  });
+  return response.items;
+}
+
+function isLink(object) {
+  return _.getPath(object, ['sys', 'type']) === 'Link';
+}
+
+function getLink(response, link) {
+  var type = link.sys.linkType;
+  var id = link.sys.id;
+  var pred = function(resource) {
+    return resource.sys.type === type && resource.sys.id === id;
+  };
+  return _.find(response.items, pred) ||
+    response.includes && _.find(response.includes[type], pred);
+}
+
+function walkMutate(input, pred, mutator) {
+  if (pred(input))
+    return mutator(input);
+
+  if (_.isArray(input) || _.isObject(input)) {
+    _.each(input, function(item, key) {
+      input[key] = walkMutate(item, pred, mutator);
+    });
+    return input;
+  }
+
+  return input;
+}
