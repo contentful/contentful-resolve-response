ccbf6ddb6303cd0d854fa0b0a1ed1ce73aa40942
Stephen Sugden <me@stephensugden.com>
2015-08-21 18:12:06 +0200

    Handle null/undefined array in find helper

--- index.js
+++ index.js
@@ -40,6 +40,9 @@ function walkMutate(input, pred, mutator) {
 }
 
 function find (array, pred) {
+  if (!array) {
+    return;
+  }
   for (var i = 0, len = array.length; i < len; i++) {
     if (pred(array[i])) {
       return array[i];
